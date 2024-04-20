e70e998730cabffece13a6f8bb7deb4b10230131
mostafa <mostafakz5256@gmail.com>
2024-04-20 09:28:51 +0330

    ...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Routes } from 'react-router-dom';
+import {Route, Routes} from "react-router-dom";
 import './App.css';
 // import Header from './Header';
 // import Post from './post';
